0b70b75ac661595f51e84ed31fd61a696d0506ec
jstalinko <alinkodotid@gmail.com>
2025-01-12 01:16:55 +0700

    Barang

--- app/Filament/Resources/BarangResource/Pages/EditBarang.php
+++ app/Filament/Resources/BarangResource/Pages/EditBarang.php
@@ -2,9 +2,10 @@
 
 namespace App\Filament\Resources\BarangResource\Pages;
 
-use App\Filament\Resources\BarangResource;
 use Filament\Actions;
+use App\Models\Variant;
 use Filament\Resources\Pages\EditRecord;
+use App\Filament\Resources\BarangResource;
 
 class EditBarang extends EditRecord
 {
@@ -16,5 +17,32 @@ class EditBarang extends EditRecord
             Actions\ViewAction::make(),
             Actions\DeleteAction::make(),
         ];
+    }
+
+    protected function mutateFormDataBeforeFill(array $data): array
+    {
+        $variant = Variant::select('stock', 'warna')->where('barang_id', $data['id'])->get();
+        $data['variasi'] = collect($variant);
+
+
+        return $data;
+    }
+
+    protected function mutateFormDataBeforeSave(array $data): array
+    {
+        
+        Variant::where('barang_id',$this->record->id)->delete();
+        
+        foreach($data['variasi'] as $var)
+        {
+            $v = new Variant();
+            $v->barang_id = $this->record->id;
+            $v->stock = $var['stock'];
+            $v->sisa_stock = $var['stock'];
+            $v->warna = $var['warna'];
+            $v->save();
+        }
+
+        return $data;
     }
 }
